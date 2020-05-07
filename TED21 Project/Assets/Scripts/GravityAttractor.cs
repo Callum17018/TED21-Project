4eee008c277d8325bab3d87fc6b14c055b5fed8a
Callum <63936790+Callum17018@users.noreply.github.com>
2020-05-07 19:17:45 +1200

    Add version 0.1

---
+++ TED21 Project/Assets/Scripts/GravityAttractor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityAttractor : MonoBehaviour
+{
+
+    public float gravity = -10f;
+    
+    public void Attract(Rigidbody body)
+    {
+        //Make vec3 varibles for rotating and position
+        Vector3 targetDirection = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.transform.up;
+
+        // Apply downwards gravity to body
+        body.AddForce(bodyUp * gravity);
+        // Change rotation so that the player is facing upwards
+        body.rotation = Quaternion.FromToRotation(bodyUp, targetDirection) * body.rotation;
+
+    }
+}
---
+++ TED21 Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(GravityBody))]
+public class PlayerController : MonoBehaviour
+{
+    // public vars
+    public float mouseSensitivityX = 1;
+    public float mouseSensitivityY = 1;
+    public float walkSpeed = 6;
+    public float jumpForce = 220;
+
+    // The ground
+    public LayerMask groundedMask;
+
+    // in here vars
+    bool grounded;
+    Vector3 moveAmount;
+    Vector3 smoothMoveVelocity;
+    float verticalLookRotation;
+    Transform cameraTransform;
+    Rigidbody rb;
+
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        cameraTransform = Camera.main.transform;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        // Changes the Camera direction based on the movement of the mouse
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
+        // Locks the vertical direction so you cant keep going up or down
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -60, 60);
+        cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
+
+        
+        float inputX = 0;
+        float inputY = 0;
+        // Gets Input keys
+        if (Input.GetKey("w"))
+        {
+            inputY += 1;
+        }
+        if (Input.GetKey("a"))
+        {
+            inputX -= 1;
+        }
+        if (Input.GetKey("s"))
+        {
+            inputY += 1;
+        }
+        if (Input.GetKey("d"))
+        {
+            inputX += 1;
+        }
+
+        // Creates a new varible to get where to go the next gameUpdate (frame)
+        Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
+        // Smooths the movement so it isnt jerky
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            if (grounded)
+            {
+                rb.AddForce(transform.up * jumpForce);
+            }
+        }
+
+        // check if player is on the ground
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1 + .1f, groundedMask))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+    }
+
+    // Runs every update (frame)
+    void FixedUpdate()
+    {
+        // Apply movement to rigidbody
+        Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + localMove);
+    }
+}
